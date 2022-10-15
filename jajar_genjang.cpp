1ebcad00776f05ea5a2a10a28274e0742f2e4247
0xtbug <tubagus.nabil19@gmail.com>
2022-10-16 03:36:30 +0700

    Upload file tugas

---
+++ jajar_genjang.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float a, t, hitungLuas;
+
+    cout << "Masukan nilai a: ";
+    cin >> a;
+
+    cout << "Masukan nilai t: ";
+    cin >> t;
+
+    hitungLuas = a * t;
+
+    cout << "Luas jajar genjang adalah: " << hitungLuas << endl;
+
+  return 0;    
+}
---
+++ persegi_panjang.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int p, l, hitungLuas;
+
+    cout << "Masukan nilai p: ";
+    cin >> p;
+
+    cout << "Masukan nilai l: ";
+    cin >> l;
+
+    hitungLuas = p * l;
+
+    cout << "Luas persegi panjang adalah: " << hitungLuas << endl;
+
+    return 0;   
+}
